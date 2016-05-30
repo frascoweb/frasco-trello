140a1792cb2cb42e2c466297f49f3600240c1a1a
maximebf <maxime.bouroumeau@gmail.com>
2016-05-30 13:03:13 +0000

    removed unused dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco-trello',
-    version='0.4',
+    version='0.4.1',
     url='http://github.com/frascoweb/frasco-trello',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
@@ -13,8 +13,6 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'frasco-users',
-        'python-dateutil==2.4.0',
-        'requests_oauthlib'
+        'frasco-users'
     ]
 )
